8995315256999e2f143f5b54c4c79d624f69ac73
amothm80 <amo.1980@gmail.com>
2024-07-02 21:26:35 +0300

    testing dom manipulation

--- js-file.js
+++ js-file.js
@@ -5,12 +5,17 @@ p.textContent ="hey i'm red";
 body.appendChild(p);
 
 let h3 = document.createElement("h3");
-h3.setAttribute("style","color: red");
+// h3.setAttribute("style","color: red");
+h3.style.color = "red";
 h3.textContent = "i'm blue h3";
 body.appendChild(h3);
 
 let div = document.createElement("div");
-div.setAttribute("style","border-style: solid; background: pink")
+// div.setAttribute("style","border-style: solid; background: pink")
+div.style.borderStyle = "solid";
+div.style.background = "pink";
+div.setAttribute("id","divid");
+div.setAttribute("class","divclass");
 body.appendChild(div);
 
 let h1 = document.createElement("h1");
